b1c8f6ab4a243a043ceef8216aab3e4165e17d4b
migasun <migasun@gmail.com>
2019-11-12 09:11:19 +0800

    Update String-1.java

--- Java/String-1.java
+++ Java/String-1.java
@@ -1,10 +1,19 @@
-// Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!". 
+
+//Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+//helloName("Bob") → "Hello Bob!"
+//helloName("Alice") → "Hello Alice!"
+//helloName("X") → "Hello X!"
+	
 public String helloName(String name)
 {	return ("Hello " + name + '!');	}
 
 
 // Given two strings, a and b, return the result of putting them together in the order abba,
-// e.g. "Hi" and "Bye" returns "HiByeByeHi".
+// e.g. "Hi" and "Bye" returns "HiByeByeHi".//
+//makeAbba("Hi", "Bye") → "HiByeByeHi"
+//makeAbba("Yo", "Alice") → "YoAliceAliceYo"
+//makeAbba("What", "Up") → "WhatUpUpWhat"
+
 public String makeAbba(String a, String b)
 {	return (a+b+b+a);	}
 
