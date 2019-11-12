40a981754470b0600c652afbbabc4ae907a64e2d
migasun <migasun@gmail.com>
2019-11-12 09:35:50 +0800

    Update String-1.java

--- Java/String-1.java
+++ Java/String-1.java
@@ -39,32 +39,51 @@ public String makeTags(String tag, String word){
 // Given an "out" string length 4, such as "<<>>", and a word, return a new string
 // where the word is in the middle of the out string, e.g. "<<word>>".
 // Note: use str.substring(i, j) to extract the String starting at index i and going up to but not including index j.
+//makeOutWord("<<>>", "Yay") → "<<Yay>>"
+//makeOutWord("<<>>", "WooHoo") → "<<WooHoo>>"
+//makeOutWord("[[]]", "word") → "[[word]]"
 public String makeOutWord(String out, String word)
 {	return (out.substring(0, 2) + word + out.substring(2, 4));	}
 
 
 // Given a string, return a new string made of 3 copies of the last 2 chars of the original string.
 // The string length will be at least 2.
+//extraEnd("Hello")	"lololo"
+//extraEnd("ab")	"ababab"
+//extraEnd("Hi")	"HiHiHi"
+//extraEnd("Candy")	"dydydy"
+//extraEnd("Code")	"dedede
 public String extraEnd(String str)
 {
-	int len = str.length();
-	String temp = str.substring(len-2, len);
-	return (temp + temp + temp);
+ 	String end = str.substring(str.length()-2);
+  	return end + end + end;
 }
 
 
 // Given a string, return the string made of its first two chars, so the String "Hello" yields "He".
 // If the string is shorter than length 2, return whatever there is, so "X" yields "X",
 // and the empty string "" yields the empty string "".Note that str.length() returns the length of a string.
+//extraEnd("Hello") → "lololo"
+//extraEnd("ab") → "ababab"
+//extraEnd("Hi") → "HiHiHi"
 public String firstTwo(String str)
 {
-	if(str.length() >= 3)
-		return str.substring(0, 2);
-	return str;
+	 if (str.length() >= 2) {
+	    return str.substring(0, 2);
+	  } else {
+	    return str;
+	  }
 }
 
 
 // Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+//firstHalf("WooHoo")	"Woo"
+//firstHalf("HelloThere")	"Hello"
+//firstHalf("abcdef")	"abc"
+//firstHalf("ab")	"a"
+//firstHalf("")	""
+//firstHalf("0123456789")	"01234"
+//firstHalf("kitten")	"kit"
 public String firstHalf(String str)
 {
 	return str.substring(0, str.length()/2);
@@ -73,12 +92,33 @@ public String firstHalf(String str)
 
 // Given a string, return a version without the first and last char, so "Hello" yields "ell".
 // The string length will be at least 2.
+//withoutEnd("Hello")	"ell"
+//withoutEnd("java")	"av"
+//withoutEnd("coding")	"odin"
+//withoutEnd("code")	"od"
+//withoutEnd("ab")	""
+//withoutEnd("Chocolate!")	"hocolate"
+//withoutEnd("kitten")	"itte"
+//withoutEnd("woohoo")	"ooho"
 public String withoutEnd(String str)
-{	return str.substring(1, str.length()-1);	}
+{
+	return str.substring(1, str.length()-1);	
+}
 
 
 // Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside
 // and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
+//comboString("Hello", "hi")	"hiHellohi"
+//comboString("hi", "Hello")	"hiHellohi"
+//comboString("aaa", "b")	"baaab"
+//comboString("b", "aaa")	"baaab"
+//comboString("aaa", "")	"aaa"
+//comboString("", "bb")	"bb"
+//comboString("aaa", "1234")	"aaa1234aaa"
+//comboString("aaa", "bb")	"bbaaabb"
+//comboString("a", "bb")	"abba"
+//comboString("bb", "a")	"abba"
+//comboString("xyz", "ab")	"abxyzab"
 public String comboString(String a, String b)
 {
 	if(a.length() >= b.length())
@@ -89,14 +129,36 @@ public String comboString(String a, String b)
 
 // Given 2 strings, return their concatenation, except omit the first char of each.
 // The strings will be at least length 1.
+//nonStart("Hello", "There")	"ellohere"
+//nonStart("java", "code")	"avaode"
+//nonStart("shotl", "java")	"hotlava"
+//nonStart("ab", "xy")	"by"
+//nonStart("ab", "x")	"b"
+//nonStart("x", "ac")	"c"
+//nonStart("a", "x")	""
+//nonStart("kit", "kat")	"itat"
+//nonStart("mart", "dart")	"artart"
+
 public String nonStart(String a, String b)
-{	return (a.substring(1) + b.substring(1));	}
+{	
+	return (a.substring(1) + b.substring(1));	
+}
 
 
 // Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end.
 // The string length will be at least 2.
+//left2("Hello")	"lloHe"
+//left2("java")	"vaja"
+//left2("Hi")	"Hi"
+//left2("code")	"deco"
+//left2("cat")	"tca"
+//left2("12345")	"34512"
+//left2("Chocolate")	"ocolateCh"
+//left2("bricks")	"icksbr"
 public String left2(String str)
-{	return (str.substring(2) + str.substring(0, 2));}
+{	
+	return (str.substring(2) + str.substring(0, 2));
+}
 
 
 // Given a string, return a "rotated right 2" version where the last 2 chars are moved to the start.
